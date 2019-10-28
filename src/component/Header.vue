b6656715ad294a43793a7a1269bf9c4533dae453
Jose Miralles <jemiralleslopez@gmail.com>
2019-10-28 06:35:47 +0000

    Modifications 28/10/19

---
+++ src/component/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>Quotes Added</h3>
+    <app-form></app-form>
+    <div class="progress">
+      <div class="progress">
+        <div
+          class="progress-bar"
+          role="progressbar"
+          aria-valuenow="60"
+          aria-valuemin="0"
+          aria-valuemax="100"
+          :style="{ width: (quoteCount / maxQuotes) * 100 + '%'}"
+        >{{ quoteCount }} / {{ maxQuotes }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Form from "./Form";
+export default {
+  props: ["quoteCount", "maxQuotes"],
+  components: {
+    appForm: Form
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
